96849dd9249aad0d1811a9b73d539bbd5cec32bd
Jackson Oppenheim <jacksonopp@gmail.com>
2019-12-03 13:38:13 -0600

    added button and touchable opacity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Wassup!</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Wassup!</Text>
+      <Button
+        title="Go to Components Demo"
+        onPress={() => { console.log("button pressed") }}
+      />
+      <TouchableOpacity
+        onPress={() => { console.log("list pressed") }}
+      >
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({

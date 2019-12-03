68a75959524154b5434f46cff923cf1a8e6f6296
Jackson Oppenheim <jacksonopp@gmail.com>
2019-12-03 14:58:28 -0600

    added counter screen

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
-const ImageDetail = ({ title, imageSource }) => {
+const ImageDetail = ({ title, imageSource, imageScore }) => {
   return (
     <View>
       <Image
         source={imageSource}
-
       />
       <Text>{title || 'no title given'}</Text>
+      <Text>{`Image Score - ${imageScore}` || 'no score given'}</Text>
     </View>
   )
 }
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -5,9 +5,18 @@ import ImageDetail from '../components/ImageDetail';
 const ImageScreen = () => {
   return (
     <View>
-      <ImageDetail title="Forest" imageSource={require("../../assets/forest.jpg")} />
-      <ImageDetail title="Beach" imageSource={require("../../assets/beach.jpg")} />
-      <ImageDetail title="Mountain" imageSource={require("../../assets/mountain.jpg")} />
+      <ImageDetail title="Forest"
+        imageSource={require("../../assets/forest.jpg")}
+        imageScore={3}
+      />
+      <ImageDetail title="Beach"
+        imageSource={require("../../assets/beach.jpg")}
+        imageScore={2}
+      />
+      <ImageDetail title="Mountain"
+        imageSource={require("../../assets/mountain.jpg")}
+        imageScore={8}
+      />
     </View>
   )
 }

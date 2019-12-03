0d3bb91e8daa2f9bd7e6b70357cf6e71ed87e16c
Jackson Oppenheim <jacksonopp@gmail.com>
2019-12-03 14:43:28 -0600

    added image components

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import ImageDetail from '../components/ImageDetail';
 
 const ImageScreen = () => {
   return (
-    <Text>ImageScreen</Text>
+    <View>
+      <ImageDetail title="Forest" imageSource={require("../../assets/forest.jpg")} />
+      <ImageDetail title="Beach" imageSource={require("../../assets/beach.jpg")} />
+      <ImageDetail title="Mountain" imageSource={require("../../assets/mountain.jpg")} />
+    </View>
   )
 }
 
